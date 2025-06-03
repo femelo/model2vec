82addb522d41ead19d1f83a39732fa04523d79a3
Flávio Eler De Melo <flavio.eler@gmail.com>
2025-06-03 17:42:46 +0200

    Fix train init

--- model2vec/train/__init__.py
+++ model2vec/train/__init__.py
@@ -5,6 +5,6 @@ _REQUIRED_EXTRA = "train"
 for extra_dependency in get_package_extras("model2vec", _REQUIRED_EXTRA):
     importable(extra_dependency, _REQUIRED_EXTRA)
 
-from model2vec.train.classifier import StaticModelForClassification
+from model2vec.train.classifier import StaticModelForClassification, StaticEnsembleModelForClassification
 
-__all__ = ["StaticModelForClassification"]
+__all__ = ["StaticModelForClassification", "StaticEnsembleModelForClassification"]
